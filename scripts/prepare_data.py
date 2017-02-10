fe30564ade03ce9c35e12311bff261cfee4be2d3
Eugene Vorontsov <eugene@imagia.com>
2017-02-10 05:28:10 -0500

    Prepare data with only slices that contain lesion.

--- scripts/prepare_data.py
+++ scripts/prepare_data.py
@@ -4,7 +4,14 @@ import os
 import zarr
 
 data_dir = "/export/projects/Candela/datasets/lits_challenge/all/"
-save_path = "/export/projects/Candela/datasets/by_project/lits/data.zarr"
+save_dir = "/export/projects/Candela/datasets/by_project/lits/"
+save_name = "data"
+include_classes = [2]
+if len(include_classes):
+    save_name += "_"+"_".join(str(i) for i in include_classes)
+save_name += ".zarr"
+save_path = os.path.join(save_dir, save_name)
+
 zgroup = zarr.open_group(store=save_path, mode='w', path="/")
 zarr_kwargs = {'chunks': (1, 512, 512),
                'compressor': zarr.Blosc(cname='lz4', clevel=9, shuffle=1)}
@@ -15,8 +22,18 @@ for i in range(130):
     volume_np = sitk.GetArrayFromImage(volume)
     seg = sitk.ReadImage(os.path.join(data_dir, "segmentation-"+str(i)+".nii"))
     seg_np = sitk.GetArrayFromImage(seg)
+    
+    if len(include_classes):
+        for c in include_classes:
+            indices = np.where(seg_np==c)
+            volume_np = volume_np[np.unique(indices[0])]
+            seg_np = seg_np[np.unique(indices[0])]
+    if len(volume_np)==0:
+        print("WARNING! Skipping empty volume #{}".format(i))
+        continue
+    
+    print("Saving {} slices".format(volume_np.shape[0]))
     subgroup = zgroup.create_group(str(i))
-    print(volume_np.shape)
     subgroup.create_dataset("volume", shape=volume_np.shape, data=volume_np,
                             dtype=np.float32, **zarr_kwargs)
     subgroup.create_dataset("segmentation", shape=seg_np.shape, data=seg_np,
