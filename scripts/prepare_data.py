2d725d4ecec484e66c9dfde4468111f23616e1c8
Unknown <gabsha.chartrand@gmail.com>
2017-02-13 14:35:40 -0500

    Add liver and background classes to training data.

--- scripts/prepare_data.py
+++ scripts/prepare_data.py
@@ -2,13 +2,57 @@ import numpy as np
 import SimpleITK as sitk
 import os
 import zarr
+import random
+import scipy.misc
 
 data_dir = "/export/projects/Candela/datasets/lits_challenge/all/"
 save_dir = "/export/projects/Candela/datasets/by_project/lits/"
-save_name = "data"
-include_classes = [2]
-if len(include_classes):
-    save_name += "_"+"_".join(str(i) for i in include_classes)
+save_name = "data_extended"
+
+# Define class inclusion percentage
+proportion_liver = 0.5
+proportion_bg = 0.3
+
+def get_slices(segm, target_class, exclude_class=None, proportion=1.0):
+    """
+    Return slices corresponding to a target segmentation class
+    
+    :param segm: Segmentation mask in numpy.array
+    :param target_class: Only slices containing this class are returned
+    :param exclude_class: Only slices excluding this class are returned
+    :param proportion: Percentage of available slices returned randomly
+    """
+
+    indices = np.where(segm==target_class)
+    slices = np.unique(indices[0])
+
+    if exclude_class is not None:
+        exclude_indices = np.where(segm==exclude_class)
+        exclude_slices = np.unique(exclude_indices[0])
+        slices = [x for x in slices if x not in exclude_slices]
+        
+    if proportion < 1.0:
+        M = list(range(len(slices)))
+        random.shuffle(M)
+        n = proportion * len(slices)
+        n = round(n)
+        slices = np.array(slices)
+        slices = slices[M[:n]]
+ 
+    return slices
+
+def save_slices(vol_np):
+    """
+    Save all slices of a given volume to JPEG in tmp/
+    
+    :param vol_np: volume to be saved 
+    """
+    for s in range(len(vol_np)):
+        scipy.misc.imsave('/tmp/test'+str(s)+'.jpg', vol_np[s])
+
+"""
+Starting the data preparation script
+"""
 save_name += ".zarr"
 save_path = os.path.join(save_dir, save_name)
 
@@ -22,12 +66,20 @@ for i in range(130):
     volume_np = sitk.GetArrayFromImage(volume)
     seg = sitk.ReadImage(os.path.join(data_dir, "segmentation-"+str(i)+".nii"))
     seg_np = sitk.GetArrayFromImage(seg)
+
+    slices = []
+    if proportion_bg > 0:
+        slices.extend(get_slices(seg_np, target_class=0, exclude_class=1, proportion=proportion_bg))
+    if proportion_liver > 0:
+        slices.extend(get_slices(seg_np, target_class=1, exclude_class=2, proportion=proportion_liver))
+    slices.extend(get_slices(seg_np, target_class=2))
+
+    volume_np = volume_np[slices]
+    seg_np = seg_np[slices]
+
+    # Sanity check
+    # save_slices(seg_np)
     
-    if len(include_classes):
-        for c in include_classes:
-            indices = np.where(seg_np==c)
-            volume_np = volume_np[np.unique(indices[0])]
-            seg_np = seg_np[np.unique(indices[0])]
     if len(volume_np)==0:
         print("WARNING! Skipping empty volume #{}".format(i))
         continue
