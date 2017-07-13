05947ef5e4345115ab1b0b14041e2f68e8fc14af
Eugene Vorontsov <eugene@imagia.com>
2017-07-13 19:31:07 -0400

    Fixed naming of loss (62a3a3aad0d72ff8423c845618cd2433d33ca4b6 was not correct).

--- lib/train.py
+++ lib/train.py
@@ -232,8 +232,8 @@ def prepare_model(model, num_classes, batch_size, val_batch_size, max_patience,
         def loss(loss_func, weight):
             def f(y_true, y_pred):
                 loss = loss_func(y_true, y_pred)*weight
-                loss.__name__ = loss_func.__name__
                 return loss
+            f.__name__ = loss_func.__name__
             return f
         masked_class = 0 if mask_to_liver else None
         losses = {}
